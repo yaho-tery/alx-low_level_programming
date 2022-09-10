bf47eb8ba58bc1332a5a3ee19823420dddafcb19
yaho-tery <terryakuks@gmail.com>
2022-09-10 04:09:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+/*
+ *File: 4-puts.c
+ *name: AKUKULONG GILBERT NKANGBUANGBUA
+ * /
+ 
+#include <stdio.h>
+
+/**
+ * main - displays "Programming is like building
+ *                a multilingual puzzle, followed by a new line"
+ *
+ *
+ *Return always: 0.
+ */
+int main(void)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+
+       return 0;
+}
