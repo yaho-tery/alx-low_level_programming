ed066679d517281ff83266592bd4bff1120286d6
yaho-tery <terryakuks@gmail.com>
2022-09-23 14:30:08 -0700

    ptve or ntve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("is positive %d\n", n);
+		printf("%d is positive\n", n);
 	if (n == 0)
-		printf("is zero %d/n", n);
+		printf("%d is zero/n", n);
 	else
-		printf("is negative %d\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
